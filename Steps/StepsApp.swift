26c358db7577c74fe664ace7b47ceed8e47c8c38
r.lepeha <r.lepeha@traderevolution.com>
2026-06-05 16:02:04 +0300

    Initial Commit

---
+++ Steps/StepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsApp.swift
+//  Steps
+//
+//  Created by Ruslan Lepekha on 05.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct StepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
